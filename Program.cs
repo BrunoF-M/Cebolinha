87f98bc893d955a40403058fbe091cd2af2d65ee
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-12 03:16:08 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string texto;
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine("Olá, eu sou o Cebolinha.. \nE vou lepetir tudo que você Escleve aqui: ");
+Console.ResetColor();
+            texto = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            string textocbl = texto.Replace("r" , "l").Replace("R" , "L");
+            Console.WriteLine(""); //espaço
+            Console.WriteLine($" {textocbl}");
+Console.ResetColor();
+            Console.WriteLine(""); //espaço
+            Console.WriteLine("Pressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+Console.ResetColor();
+            Console.Clear();
+        }
+    }
+}
